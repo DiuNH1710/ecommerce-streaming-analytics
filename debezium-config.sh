c94cd69567e3021911335001a87857f79ea890e7
Nguyen Huong Diu <nguyenhuongdiu1710@gmail.com>
2025-09-08 14:08:17 +0700

    add docker file and debezium config

---
+++ debezium-config.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Debezium REST API
+DEBEZIUM_URL="http://localhost:8083/connectors"
+
+# Function to create connector for a table
+create_connector() {
+  local connector_name=$1
+  local table_name=$2
+
+  curl -X POST -H "Content-Type: application/json" $DEBEZIUM_URL --data "{
+    \"name\": \"$connector_name\",
+    \"config\": {
+      \"connector.class\": \"io.debezium.connector.postgresql.PostgresConnector\",
+      \"plugin.name\": \"pgoutput\",
+      \"database.hostname\": \"postgres\",
+      \"database.port\": \"5432\",
+      \"database.user\": \"postgres\",
+      \"database.password\": \"postgres\",
+      \"database.dbname\": \"ecommerce_db\",
+      \"database.server.name\": \"postgres\",
+      \"table.include.list\": \"public.$table_name\",
+      \"topic.prefix\": \"cdc\",
+      \"decimal.handling.mode\": \"string\"
+    }
+  }"
+  echo -e "\nConnector $connector_name created for table $table_name"
+}
+
+# 1. Cloud warehouse comparison
+create_connector "cloud_warehouse_comparison_conn" "cloud_warehouse_comparison"
+
+# 2. Sale report
+create_connector "sale_report_conn" "sale_report"
+
+# 3. P & L March 2021
+create_connector "pnl_march_2021_conn" "pnl_march_2021"
+
+# 4. May 2022
+create_connector "may_2022_conn" "may_2022"
+
+# 5. Amazon Sale Report
+create_connector "amazon_sale_report_conn" "amazon_sale_report"
+
+# 6. International Sale Report
+create_connector "international_sale_report_conn" "international_sale_report"
+
+# 7. Expense IIGF
+create_connector "expense_iigf_conn" "expense_iigf"
